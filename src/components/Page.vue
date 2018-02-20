5dc5d4132e885850e3ac02277d441ae734151220
Colin Cybergraph <colin@cybergraph.fr>
2018-02-20 14:14:47 +0100

    One component for all pages

---
+++ src/components/Page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ $t($route.name + '.title') }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import About from '@/components/About'
-import Works from '@/components/Works'
+
+import Page from '@/components/Page'
 import Error from '@/components/Error'
 
 Vue.use(Router)
@@ -10,10 +9,10 @@ Vue.use(Router)
 const router = new Router({
   mode: 'history',
   routes: [
-    { path: '/', redirect: '/fr' },
-    { path: '/:lang/', name: 'home', component: Home },
-    { path: '/:lang/works', alias: ['/:lang/travaux', '/:lang/works-de'], name: 'works', component: Works },
-    { path: '/:lang/about', alias: ['/:lang/a-propos', '/:lang/about-de'], name: 'about', component: About },
+    { path: '/', redirect: '/en' },
+    { path: '/:lang/', name: 'home', component: Page },
+    { path: '/:lang/works', alias: ['/:lang/travaux'], name: 'works', component: Page },
+    { path: '/:lang/about', alias: ['/:lang/a-propos'], name: 'about', component: Page },
     { path: '/:lang/*', name: 'page-404', component: Error }
   ]
 })
