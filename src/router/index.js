8d3585155b1f34f86bb425d47ebab892cd84c19a
Colin Cybergraph <colin@cybergraph.fr>
2018-02-20 14:13:12 +0100

    Add 404 check

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
 import Works from '@/components/Works'
+import Error from '@/components/Error'
 
 Vue.use(Router)
 
@@ -12,7 +13,8 @@ const router = new Router({
     { path: '/', redirect: '/fr' },
     { path: '/:lang/', name: 'home', component: Home },
     { path: '/:lang/works', alias: ['/:lang/travaux', '/:lang/works-de'], name: 'works', component: Works },
-    { path: '/:lang/about', alias: ['/:lang/a-propos', '/:lang/about-de'], name: 'about', component: About }
+    { path: '/:lang/about', alias: ['/:lang/a-propos', '/:lang/about-de'], name: 'about', component: About },
+    { path: '/:lang/*', name: 'page-404', component: Error }
   ]
 })
 
