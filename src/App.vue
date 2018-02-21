a9af40ba1bd8d1d36b923c9fbc2e95799fefbaf8
Colin Cybergraph <colin@cybergraph.fr>
2018-02-21 09:16:21 +0100

    Navigation transition on language switch

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <nav>
       <switcher></switcher>
-      <navigation></navigation>
+      <transition name="navigation" mode="out-in">
+        <navigation :key="$t($route.params.lang + '.title')"></navigation>
+      </transition>
     </nav>
-    <transition name="fade">
-      <router-view class="container"></router-view>
+    <transition name="view" mode="out-in">
+      <router-view :key="$t($route.name + '.title')"></router-view>
     </transition>
   </div>
 </template>
@@ -25,13 +27,17 @@ export default {
 
 <style>
   * { box-sizing: border-box; margin: 0; border: 0; padding: 0; list-style: none; }
-  html, body { height: 100%; }
+  html, body { height: 100%; overflow-x: hidden; }
   body { padding: 32px; background: #fff; color: #000; font-family: Helvetica, Arial, sans-serif; font-weight: normal; font-style: normal; font-size: 16px; line-height: 1.5; }
   a { color: #000; text-decoration: none; }
   nav:after { content: ''; display: block; clear: both; }
   h1 { margin-top: 64px; font-size: 64px; clear: both; }
-  /* transition router-view */
-  .fade-enter-active, .fade-leave-active { transition-property: opacity; transition-duration: .20s; }
-  .fade-enter-active { transition-delay: .20s; }
-  .fade-enter, .fade-leave-active { opacity: 0; }
+
+  .navigation-enter-active, .navigation-leave-active { transition: all 0.25s ease; }
+  .navigation-enter, .navigation-leave-to { transform: translateY(-64px); }
+
+  .view-enter-active, .view-leave-active { transition: all 0.25s ease; }
+  .view-enter, .view-leave-to { opacity: 0; }
+  .view-enter { transform: scale(1.5); }
+  .view-leave-to { transform: scale(0.5); }
 </style>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="page">
     <h1>{{ $t($route.name + '.title') }}</h1>
   </div>
 </template>
